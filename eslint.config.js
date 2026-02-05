0e44696fad08805189384b9d9ec3b778d65cbbae
yungblud <dongho.choi@minishtech.com>
2026-02-05 09:36:41 +0900

    Add Prettier and ESLint configuration files, update package.json scripts, and add .prettierignore

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,20 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintConfigPrettier = require('eslint-config-prettier');
+const eslintPluginPrettier = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
   expoConfig,
+  eslintConfigPrettier,
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
   {
     ignores: ['dist/*'],
   },
