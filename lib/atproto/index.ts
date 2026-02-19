4d9d99ac50326814ee945d06deb854277f7b12e5
yungblud <dongho.choi@minishtech.com>
2026-02-19 11:57:20 +0900

    chore: lint fix

--- lib/atproto/index.ts
+++ lib/atproto/index.ts
@@ -1,15 +1,15 @@
-import { AtpAgent } from "@atproto/api";
+import { AtpAgent } from '@atproto/api';
 
 export const Agent = new AtpAgent({
-    service: "https://bsky.social",
-})
+  service: 'https://bsky.social',
+});
 
 export const getTimeline = async () => {
-    try {
-        const response = await Agent.getTimeline({ limit: 20 });
-        return response
-      } catch (error) {
-        console.error('Failed to load timeline:', error);
-        return null
-      }
-}
+  try {
+    const response = await Agent.getTimeline({ limit: 20 });
+    return response;
+  } catch (error) {
+    console.error('Failed to load timeline:', error);
+    return null;
+  }
+};
